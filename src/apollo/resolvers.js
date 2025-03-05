a6aaa1761998449a77d9552ab570b7a7316bf01c
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-05 12:48:33 -0300

    added sequelize sync to resolver

--- src/apollo/resolvers.js
+++ src/apollo/resolvers.js
@@ -1,13 +1,13 @@
-// import { sequelize } from "../database/connection.js"
+import { sequelize } from '../database/connection.js'
 import { User } from '../database/models/User.js'
 
 export const resolvers = {
   Query: {
     hello: async () => {
-      // await sequelize.sync()
+      await sequelize.sync()
 
       const user = await User.create({
-        email: 'e@mail.com'
+        email: 'e@mail.com',
       })
 
       return `User (${user.email}) created successfully `
