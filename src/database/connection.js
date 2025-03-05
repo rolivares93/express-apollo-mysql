80030c6d3c56a83af55fea4419c37bed33cc3879
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-05 12:26:05 -0300

    added new config to sequelize

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,6 +5,7 @@ export const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD, {
     dialect: process.env.DB_DRIVER,
+    dialectModule: require(process.env.DB_DRIVER),
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     logging: false
