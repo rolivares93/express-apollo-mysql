db7f873285c6bc0810e3adc9e4af0f9a4c8c4a01
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-05 12:44:35 -0300

    updated resolver to create user

--- src/apollo/resolvers.js
+++ src/apollo/resolvers.js
@@ -1,5 +1,16 @@
+// import { sequelize } from "../database/connection.js"
+import { User } from '../database/models/User.js'
+
 export const resolvers = {
   Query: {
-    hello: () => 'Hello, World',
+    hello: async () => {
+      // await sequelize.sync()
+
+      const user = await User.create({
+        email: 'e@mail.com'
+      })
+
+      return `User (${user.email}) created successfully `
+    },
   },
 }
