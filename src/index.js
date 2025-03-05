f80158b9540d855cc591fed75683451b6895cac1
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-05 11:51:30 -0300

    removed sequelize starters

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import { expressMiddleware } from '@apollo/server/express4'
 import cors from 'cors'
 import express from 'express'
 import { server } from './apollo/server.js'
-import { sequelize } from './database/connection.js'
+// import { sequelize } from './database/connection.js'
 
 const app = express()
 const port = process.env.PORT || 4321
 
-try {
-  await sequelize.authenticate()
-  console.log('Connection has been established successfully')
-} catch (error) {
-  console.error('Unable to connect to the database:', error)
-}
+// try {
+//   await sequelize.authenticate()
+//   console.log('Connection has been established successfully')
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error)
+// }
 
 await server.start()
-await sequelize.sync()
+// await sequelize.sync()
 
 app.use(
   '/graphql',
