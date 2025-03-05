fded2964c035e4d78770b1fd085aa08bebae7ce1
Rodrigo Olivares <rolivares6793@gmail.com>
2025-03-05 12:33:14 -0300

    updated sequelize starter function

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,24 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from '@sequelize/core'
+import { MariaDbDialect } from '@sequelize/mariadb'
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD, {
-    dialect: process.env.DB_DRIVER,
-    dialectModule: require(process.env.DB_DRIVER),
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    logging: false
-  }
-)
+// export const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD, {
+//     dialect: MariaDbDialect,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     logging: false
+//   }
+// )
+
+export const sequelize = new Sequelize({
+  dialect: MariaDbDialect,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  showWarnings: true,
+  connectTimeout: 1000,
+})
